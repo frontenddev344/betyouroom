4c2ba8c3243a5c6bb195be16d9c12f946eb485ce
frontenddev344 <frontenddev344@gmail.com>
2024-08-13 13:12:23 +0530

    fixed

--- cz/assets/js/index.js
+++ cz/assets/js/index.js
@@ -16,19 +16,19 @@ $('.home-page-cards-wrapper').slick({
   infinite: true,
   slidesToShow: 3,
   slidesToScroll: 1,
-  autoplay:true,
-  dots:true,
+  autoplay: true,
+  dots: true,
   autoplaySpeed: 1500,
-   responsive: [
-       {
-      breakpoint:1900 ,
+  responsive: [
+    {
+      breakpoint: 1900,
       settings: {
         slidesToShow: 3,
         slidesToScroll: 1,
         infinite: true,
         centerMode: true,
         centerPadding: '80px',
-             }
+      }
     },
     {
       breakpoint: 991,
@@ -38,7 +38,7 @@ $('.home-page-cards-wrapper').slick({
         infinite: true,
         centerMode: true,
         centerPadding: '50px',
-             }
+      }
     },
     {
       breakpoint: 767,
@@ -50,7 +50,7 @@ $('.home-page-cards-wrapper').slick({
     {
       breakpoint: 575,
       settings: {
-        slidesToShow:2,
+        slidesToShow: 2,
         slidesToScroll: 1
       }
     },
@@ -67,7 +67,7 @@ $('.home-page-cards-wrapper').slick({
         slidesToShow: 1,
         slidesToScroll: 1
       }
-    }     
+    }
   ]
 });
 // Slick Slider JS End
@@ -89,60 +89,15 @@ $('.subpage-readmore-btn').click(function (e) {
 
 // //Cookies JS Start
 
-function SetCookie(cookieName, cookieValue, nDays) {
-  "use strict";
-  var today = new Date();
-  var expire = new Date();
-  if (nDays == null || nDays == 0) nDays = 1;
-  expire.setTime(today.getTime() + 3600000 * 24 * nDays);
-  document.cookie =
-    cookieName +
-    "=" +
-    escape(cookieValue) +
-    ";expires=" +
-    expire.toGMTString() +
-    "; path=/";
-}
-function ReadCookie(cookieName) {
-  "use strict";
-  var theCookie = " " + document.cookie;
-  var ind = theCookie.indexOf(" " + cookieName + "=");
-  if (ind == -1) ind = theCookie.indexOf(";" + cookieName + "=");
-  if (ind == -1 || cookieName == "") return "";
-  var ind1 = theCookie.indexOf(";", ind + 1);
-  if (ind1 == -1) ind1 = theCookie.length;
-  return unescape(theCookie.substring(ind + cookieName.length + 2, ind1));
-}
-function DeleteCookie(cookieName) {
-  "use strict";
-  var today = new Date();
-  var expire = new Date() - 30;
-  expire.setTime(today.getTime() - 3600000 * 24 * 90);
-  document.cookie =
-    cookieName + "=" + escape(cookieValue) + ";expires=" + expire.toGMTString();
-}
-function AcceptCookies() {
-  SetCookie("sgCookies", true, 30);
-  jQuery("#cookie-bar").hide();
-}
-$(document).ready(function (e) {
-  var toTop = $(
-    "<div id='cookie-bar'><div id='contentcookie'><p>    BetYouRoom is your premier hotel comparison site for the Czech Republic. To enhance your browsing experience and provide you with tailored services, we use cookies on our website. If you would like to know more information about Cookie Policy, please review our Privacy Policy there you will find a better explanation regarding our cookies.</p><button id='Cookie' tabindex=1 onclick='AcceptCookies();'>Accept </button><div class='clear'></div></div></div>"
-  ).appendTo("body");
-  if (!ReadCookie("sgCookies")) {
-    //If the cookie has not been set
-    jQuery("#cookie-bar").show();
-  } else {
-    jQuery("#cookie-bar").hide();
-  }
+$("#cookie-popup button").click(function () {
+  $("#cookie-popup").fadeOut();
 });
-
 // //Cookies JS Start
 
 
 
-$(document).ready(function(){
-  $(".button-dropdown").click(function(){
+$(document).ready(function () {
+  $(".button-dropdown").click(function () {
     $(this).find(".dropdown").slideToggle();
   });
 });
